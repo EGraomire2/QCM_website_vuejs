1a50ed47e3088784a5f0045208c06f127f86217f
EGromaire <etienne.gromaire@efrei.net>
2025-11-22 12:21:17 +0800

    mdp BDD

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({ 
     host: 'localhost', 
     user: 'root', 
-    password: 'Agent_cooky77', 
+    password: '', 
     database: 'project_qcm' 
 }); 
 
